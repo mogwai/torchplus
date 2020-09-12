6b252a3e8b65fccab6e5105a9f5f30ce32a538fb
Harry <himion0@gmail.com>
2020-09-12 08:25:34 +0100

    Removed version from setup

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-with open(".version") as f:
-    version = f.read()
-
 setup(
     name="torchplus",
-    version=version,
+    version="0.1.0",
     packages=find_packages(),
     description="Useful extras when working with pytorch",
     install_requires=requirements,
