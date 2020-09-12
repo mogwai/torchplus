79fa00210baa91565728bd2cc9729731ba36e6b3
Harry <himion0@gmail.com>
2020-09-12 08:25:44 +0100

    Make a noise

--- torchplus/nb.py
+++ torchplus/nb.py
@@ -1,12 +1,12 @@
 from gc import collect
-from torch.cuda import empty_cache
 
+import numpy as np
 import torch
 from torch import Tensor
+from torch.cuda import empty_cache
 from torchvision.transforms.functional import to_pil_image
-import numpy as np
 
-__all__ = ["dp", "clear_memory", "T"]
+__all__ = ["dp", "clear_memory", "T", ]
 
 
 def dp(im):
@@ -37,6 +37,69 @@ def clear_memory():
     except:
         pass
 
+def notebook_sound(url= "https://flukeout.github.io/simple-sounds/sounds/coin.mp3", volume=1):
+        from IPython.display import HTML
+        
+        script = """
+<script>
+var sounds = {
+    "coin" : {
+        url : '"""+url+"""'
+    }
+};
+
+var soundContext = new AudioContext();
+
+for(var key in sounds) {
+    loadSound(key);
+}
+
+function loadSound(name){
+    var sound = sounds[name];
+
+    var url = sound.url;
+    var buffer = sound.buffer;
+
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function() {
+        soundContext.decodeAudioData(request.response, function(newBuffer) {
+            sound.buffer = newBuffer;
+        });
+    }
+    request.send();
+}
+
+function playSound(name, custumVolume){
+    var sound = sounds[name];
+    var soundVolume = sounds[name].volume || 1;
+
+    var buffer = sound.buffer;
+    if(buffer) {
+
+        var source = soundContext.createBufferSource();
+        source.buffer = buffer;
+
+        var volume = soundContext.createGain();
+
+        if (custumVolume) {
+            volume.gain.value = soundVolume * custumVolume
+        } else {
+            volume.gain.value = soundVolume;
+        }
+
+        volume.connect(soundContext.destination);
+        source.connect(volume);
+        source.start(0);
+    }
+}
+
+setTimeout(()=>playSound('coin',"""+str(volume)+"""), 200)
+</script>
+        """
+        from IPython.display import HTML
+        display(HTML(script))
 
 # Easy access to make a tensor quickly
 T = torch.tensor
